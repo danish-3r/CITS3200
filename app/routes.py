fdaab84914f4cb209b6e8f2d4496ad9a12be4bbd
seventyD <dylanfellows990@gmail.com>
2021-08-22 22:03:44 +0800

    Setup

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from sqlalchemy.sql.expression import desc
+from app import app, db, bcrypt
+from flask import Flask, render_template, flash, redirect, url_for, request
+#from app.forms import Form
+#from app.models import User
+from datetime import datetime
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import func
+
+
+
+from app import app
+from flask import Flask, render_template
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+    
+@app.route('/home')
+def home():
+    return render_template("templates/home.html")
