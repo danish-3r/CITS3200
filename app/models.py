35aab673fa0dafae4f7d303cfe9415587743980b
ndkhiem3010 <ndkhiem3010@gmail.com>
2021-09-09 17:51:43 +0800

    Update schema

--- app/models.py
+++ app/models.py
@@ -1,126 +1,204 @@
 from app import db
 from datetime import datetime
 
-# Cluster Fee model
-class ClusterFee(db.Model):
 
-    __tablename__ = 'cluster_fee'
+### NEW SCHEMA
 
-    # Year
-    year = db.Column(db.String(64), primary_key=True)
+# Unit fee model for domestic student
+class UnitFee(db.Model):
+    __tablename__ = "unit_fee"
 
-    # Cluster
-    cluster = db.Column(db.Integer, primary_key = True)
+    # Unit code is PK
+    unit_code = db.Column(db.String(64), primary_key=True)
 
-    # Fee
-    fee = db.Column(db.Integer, index=True)
+    unit_fee = db.Column(db.Integer, index=True)
 
     def __repr__(self):
-        return '<Cluster id: {}>'.format(self.id)
+        return '<unit code: {}>'.format(self.unit_code)
 
-# Unit database model
-class Unit(db.Model):
+# Course name model stored the course code and associated coursename
+class Course(db.Model):
+    __tablename__ = "course_name"
 
-    # unit id
-    id = db.Column(db.Integer, primary_key=True)
+    # Course code is PK
+    course_code = db.Column(db.String(64), primary_key=True)
 
-    # unit code 
-    unit_cd = db.Column(db.String(64), index=True,
-    unique=True)
+    # Course name
+    course_name = db.Column(db.String(64), index=True)
 
-    # unit name
-    title = db.Column(db.String(128), index=True, unique=True)
-
-    # version??
-    version = db.Column(db.Integer)
-
-    # Unit points
-    points = db.Column(db.Integer)
-
-    # FOE
-    foe = db.Column(db.Integer)
-
-    # Domestic Cluster
-    dom_cl = db.Column(db.Integer, db.ForeignKey('cluster_fee.cluster'))
-
-    # Non Award Cluster
-    no_award_cl = db.Column(db.Integer,db.ForeignKey('cluster_fee.cluster'))
-
-    # Inter On Shore Cluster
-    inter_cl = db.Column(db.Integer,db.ForeignKey('cluster_fee.cluster'))
-
-    # New census date
-    census_date = db.Column(db.Date)
+    # Course fee
+    course_fee = db.Column(db.Integer, index=True)
 
     def __repr__(self):
-        return '<Unit id: {}>'.format(self.id)
+        return '<course code: {}>'.format(self.course_code)
 
-# Course Fee Types Model
-class CourseFeeTypes(db.Model):
+# # Course fee used for both domestic and internation
+# class CourseFee(db.Model):
+#     __tablename__ = "course_fee"
+
+#     # Course code is FK
+#     code = db.Column(db.String(64), db.ForeignKey('course_name.course_code'))
+
+#     fee = db.Column(db.Integer, index=True)
+
+#     def __repr__(self):
+#         return '<course code: {}>'.format(self.course_code)
+
+class StudentType(db.Model):
+    __tablename__ = "student_type"
     
-    # course code
-    course_code = db.Column(db.Integer, index=True, primary_key=True)
+    # Level of study - UG, PG, HDR
+    level = db.Column(db.String(64), primary_key=True)
 
-    # fee type code
-    fee_type_code = db.Column(db.Integer, index=True, primary_key=True)
+    # location - domestic or international
+    location = db.Column(db.String(64), primary_key=True)
 
     def __repr__(self):
-        return '<Course fee types id: {}>'.format(self.id)
+        return '<student type: {} - {}>'.format(self.level, self.location)
 
+class Dropdowns(db.Model):
+    __tablename__ = "dropdowns"
 
-# Course Fee Model
-class CourseFees(db.Model):
-
-    # fee type code
-    fee_type_code = db.Column(db.Integer, primary_key=True)
-
-    # course code
-    course_code = db.Column(db.Integer, primary_key=True)
-
-    # year
-    year = db.Column(db.Integer)
-
-    # fee
-    fee = db.Column(db.Integer)
-
-
-# Course Model
-class Courses(db.Model):
-    
-    # course code
-    course_code = db.Column(db.Integer, index=True, primary_key=True)
-
-    course_name = db.Column(db.String(128), unique=True)
-
-    version = db.Column(db.Integer)
-
-    # credit point
-    credit = db.Column(db.Integer)
-
-    # required credit point
-    req_credit = db.Column(db.Integer)
-
-    # course type code
-    ctc = db.Column(db.Integer)
-
-    # broad course type code
-    broad_ctc = db.Column(db.String(32))
-
-# Study Abroad Fees Model
-class StudyAbroadFees(db.Model):
-
+    # combination id - PK
     id = db.Column(db.Integer, primary_key=True)
+    
+    level = db.Column(db.String(64), db.ForeignKey('student_type.level'))
 
-    # fee type code
-    fee_type_code = db.Column(db.Integer, index=True)
+    location =  db.Column(db.String(64), db.ForeignKey('student_type.location'))
 
-    # course code
-    course_code = db.Column(db.Integer, index=True)
+    course_code =  db.Column(db.String(64), db.ForeignKey('course_name.course_code'))
 
-    # course name
-    course_name = db.Column(db.String(128))
+    # Year of study
+    years = db.Column(db.Integer, index=True)
 
-    # year
-    year = db.Column(db.Integer)
+    def __repr__(self):
+        return '<id: {} - level: {} - location: {} - course_code: {} - years: {}>'.format(self.id, self.level, self.location, self.course_code, self.years)
 
-    fee = db.Column(db.Integer)
+
+
+
+### OLD SCHEMA
+
+# # Cluster Fee model
+# class ClusterFee(db.Model):
+
+#     __tablename__ = 'cluster_fee'
+
+#     # Year
+#     year = db.Column(db.String(64), primary_key=True)
+
+#     # Cluster
+#     cluster = db.Column(db.Integer, primary_key = True)
+
+#     # Fee
+#     fee = db.Column(db.Integer, index=True)
+
+#     def __repr__(self):
+#         return '<Cluster id: {}>'.format(self.id)
+
+# # Unit database model
+# class Unit(db.Model):
+
+#     # unit id
+#     id = db.Column(db.Integer, primary_key=True)
+
+#     # unit code 
+#     unit_cd = db.Column(db.String(64), index=True,
+#     unique=True)
+
+#     # unit name
+#     title = db.Column(db.String(128), index=True, unique=True)
+
+#     # version??
+#     version = db.Column(db.Integer)
+
+#     # Unit points
+#     points = db.Column(db.Integer)
+
+#     # FOE
+#     foe = db.Column(db.Integer)
+
+#     # Domestic Cluster
+#     dom_cl = db.Column(db.Integer, db.ForeignKey('cluster_fee.cluster'))
+
+#     # Non Award Cluster
+#     no_award_cl = db.Column(db.Integer,db.ForeignKey('cluster_fee.cluster'))
+
+#     # Inter On Shore Cluster
+#     inter_cl = db.Column(db.Integer,db.ForeignKey('cluster_fee.cluster'))
+
+#     # New census date
+#     census_date = db.Column(db.Date)
+
+#     def __repr__(self):
+#         return '<Unit id: {}>'.format(self.id)
+
+# # Course Fee Types Model
+# class CourseFeeTypes(db.Model):
+    
+#     # course code
+#     course_code = db.Column(db.Integer, index=True, primary_key=True)
+
+#     # fee type code
+#     fee_type_code = db.Column(db.Integer, index=True, primary_key=True)
+
+#     def __repr__(self):
+#         return '<Course fee types id: {}>'.format(self.id)
+
+
+# # Course Fee Model
+# class CourseFees(db.Model):
+
+#     # fee type code
+#     fee_type_code = db.Column(db.Integer, primary_key=True)
+
+#     # course code
+#     course_code = db.Column(db.Integer, primary_key=True)
+
+#     # year
+#     year = db.Column(db.Integer)
+
+#     # fee
+#     fee = db.Column(db.Integer)
+
+
+# # Course Model
+# class Courses(db.Model):
+    
+#     # course code
+#     course_code = db.Column(db.Integer, index=True, primary_key=True)
+
+#     course_name = db.Column(db.String(128), unique=True)
+
+#     version = db.Column(db.Integer)
+
+#     # credit point
+#     credit = db.Column(db.Integer)
+
+#     # required credit point
+#     req_credit = db.Column(db.Integer)
+
+#     # course type code
+#     ctc = db.Column(db.Integer)
+
+#     # broad course type code
+#     broad_ctc = db.Column(db.String(32))
+
+# # Study Abroad Fees Model
+# class StudyAbroadFees(db.Model):
+
+#     id = db.Column(db.Integer, primary_key=True)
+
+#     # fee type code
+#     fee_type_code = db.Column(db.Integer, index=True)
+
+#     # course code
+#     course_code = db.Column(db.Integer, index=True)
+
+#     # course name
+#     course_name = db.Column(db.String(128))
+
+#     # year
+#     year = db.Column(db.Integer)
+
+#     fee = db.Column(db.Integer)
