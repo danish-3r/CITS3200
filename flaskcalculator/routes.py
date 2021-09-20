3019a56e3d4a24064254fa51001ecb33b0937408
Danish <danish3r@gmail.com>
2021-09-20 11:40:29 +0800

    added fee result html+css, and boostrap cdn

--- flaskcalculator/routes.py
+++ flaskcalculator/routes.py
@@ -12,11 +12,11 @@ def hello():
 @app.route('/dropdown', methods=["POST"])
 def dropdown():
     req = request.get_json() #the request data(location, level) sent from user using dropdown.js
-
+    print(req, "AEGGgguvugiuygiygiygiuygiuyguyg")
     filteredCourses = Course.query.filter_by(location=req["location"], level=req["level"]).all() #returns list of courses that match location and level
     
     coursesList = [c.course_name for c in filteredCourses] #list comprehensoin to extract course name
 
     res = make_response( jsonify({"courseList":coursesList}) ) #make a reponse and send it back to the dropdown.js
-    
+    print(res)
     return res #return a json containing list of courses
