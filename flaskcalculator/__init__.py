77a2f1091a2286c1f697548e7bf4883d147617a3
ndkhiem3010 <ndkhiem3010@gmail.com>
2021-09-22 12:00:40 +0800

    add flaskenv

--- flaskcalculator/__init__.py
+++ flaskcalculator/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Secret shhhh'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+migrate = Migrate(app, db)
 
 from flaskcalculator import routes, models
