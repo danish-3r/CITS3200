cc8c479fcebc0944177e245453905b3a7c4eca00
seventyD <dylanfellows990@gmail.com>
2021-10-05 22:23:40 +0800

    added course planner functionality

--- flaskcalculator/static/javascripts/course_planner.js
+++ flaskcalculator/static/javascripts/course_planner.js
@@ -1,6 +1,13 @@
+//the way i iterate each point in the table may not be the best (i should probably find a way to iterate the table)
+//but it works well for this situation
+//each list item id has a number after it
+//so when i call the function i give it that number since using 'self' was out of the scope
+
+
+//enters dropdown mode
 function changeto_drop(list_number)
 {   
-  
+    
     let unit_num = "+unit".concat(list_number);
     let drop_num = "planner_drop".concat(list_number);
 
@@ -8,6 +15,8 @@ function changeto_drop(list_number)
     document.getElementById(drop_num).style.visibility = "visible"; 
 
 }
+
+//exits dropdown mode
 function changeto_add(list_number)
 {
 
@@ -18,12 +27,40 @@ function changeto_add(list_number)
     document.getElementById(drop_num).style.visibility = "hidden"; 
 
 }
+
+//main function that sets up the planner
 function major_change()
 {
+    //changes title of planner to the correct major
     major_selector = document.getElementById("major_select");
     document.getElementById("major_name").innerHTML = "MAJOR: ".concat(major_selector.value);
+
+    //this should be changed to read the db and get all compulsory units for the major in a list
+    var compusory_units = []
+    if (major_selector.value == "Computer Science")
+    {
+        compusory_units = ["Introduction to Computer Science", "Relational Database Management", "How to grow a beard"]
+    }
+    if (major_selector.value == "Data Science")
+    {
+        compusory_units = ["Introduction to Data Science", "Relational Database Management", "Free robux 2021 working may"]
+
+    }
+
+    //since we dont get the start times of the compulsory units, 
+    //it may be best to just have them 1st then have the electives next
+    //so this loop will go through the compulsory units and enter them into the planner
+    for(let i = 1; i < compusory_units.length+1; i++)
+    {
+        let unit_num = "+unit".concat(i);
+        console.log(i)
+        document.getElementById(unit_num).innerHTML = compusory_units[i-1]; 
+
+    }
 }
 
+
+//called when an elective unit has been selected
 function cp_change_prices(list_number)
 {
  
