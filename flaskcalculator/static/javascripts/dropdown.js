ea8bd11c896d9d0a07a3f6fac3a8edac342f63d8
Austin <ndkhiem3010@gmail.com>
2021-10-07 07:27:05 +0800

    fix dropdown and fee result bug; update test

--- flaskcalculator/static/javascripts/dropdown.js
+++ flaskcalculator/static/javascripts/dropdown.js
@@ -1,5 +1,3 @@
-
-
 function dropDownChange()
 {
     var type = document.getElementById("locations") //get value stored in option tag
@@ -30,14 +28,9 @@ function dropDownChange()
     .then(response => response.json()) //convert response into json
     .then 
     ((
-        
-        
         data => //extract json data
-
         {
             var options =  '<option disabled="" selected="" hidden="">Select</option>' //update innerhtml of coursed dropdown
-            var id = 1;
-            console.log(data.courseList[0]);
             
             for(course of data.courseList )
             {
@@ -48,87 +41,4 @@ function dropDownChange()
         
     ))
 
-}
-
-
-function name_change()
-{
-    var course_name = document.getElementById("course_name")
-    var name_result = ""
-    var locatoin = document.getElementById("locations") //get value stored in option tag
-    var level = document.getElementById("levels")
-    var year = document.getElementById("years")
-    
-    var filter = //create a json data
-    {
-        location : locatoin.value,
-        level : level.value,
-        year : year.value
-    }
-
-    fetch //send 
-    (
-        "/dropdown", //the route which the user inputs are sent to and processed
-        {method:"POST", 
-        headers:{"Accept":"application/json", "Content-Type":"application/json"}, //the type of data being sent, so the flask application knows
-        body: JSON.stringify(filter)} //convert filter to correct data format
-    )
-    .then(response => response.json()) //convert response into json
-    .then 
-    ((
-        
-        
-        data => //extract json data
-
-        {
-            var options =  '<option disabled="" selected="" hidden="">Select</option>' //update innerhtml of coursed dropdown
-            var id = 1;
-            name_result = data.courseList[0];
-            course_name.innerHTML = name_result;
-            price_change(name_result);
-
-            for(course of data.courseList )
-            {
-                options += '<option id='+String(id)+" value='"+String(course)+"'>"+course+'</option>';
-                id++
-            }
-        }
-    ))
-
-}
-function year_change()
-{
-    var year = document.getElementById("years")
-
-    var year_result = document.getElementById("course_year")
-    year_result.innerHTML = year.value;
-
-
-}
-
-function price_change(name_result)
-{
-
-    var CCP = document.getElementById("CCP");
-    var ACP = document.getElementById("ACP");
-    var ANF = document.getElementById("ANF");
-    var TFR = document.getElementById("TFR");
-
-    switch(name_result)
-    {
-        case "Master of Education - Thesis & Coursework":
-            CCP.innerHTML = "144";
-            ACP.innerHTML = "48";
-            ANF.innerHTML = "$8,747";
-            TFR.innerHTML = "$3,950-$14,500";
-            break;
-
-        case "Bachelor of Science":
-            CCP.innerHTML = "144";
-            ACP.innerHTML = "48";
-            ANF.innerHTML = "$6,294";
-            TFR.innerHTML = "$3,451-$10,745";
-
-
-    }
 }
--- system_test.py
+++ system_test.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from flaskcalculator import app, db
-import unittest, os, time
+import unittest, os, time, logging
 from selenium.webdriver.support.ui import Select
+
+
 
 # get the path of ChromeDriverServer
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -12,6 +14,9 @@ class SystemTest(unittest.TestCase):
         # db.init_app(app)
         # db.create_all()
 
+        logging.disable(logging.CRITICAL)
+        unittest.TestLoader.sortTestMethodsUsing = None
+        
         # Windows chromedriver
         chromedriver = os.path.join(basedir, 'drivers','chromedriver_win32', 'chromedriver.exe')
         print(chromedriver)
@@ -35,52 +40,61 @@ class SystemTest(unittest.TestCase):
             self.driver.close()
 
 
-    # def test_dropdowns(self):
+    def test_dropdowns_bachelor(self):
         
-    #     # Select type of student
-    #     locations = Select(self.driver.find_element_by_id('locations'))  # find the locations menu 
-    #     locations_value = locations.select_by_visible_text('Domestic')
-    #     time.sleep(1)
-    #     self.driver.implicitly_wait(5)
+        # Select type of student
+        locations = Select(self.driver.find_element_by_id('locations'))  # find the locations menu 
+        locations_value = locations.select_by_visible_text('Domestic')
+        time.sleep(1)
+        self.driver.implicitly_wait(5)
 
-    #     # Select level of study
-    #     levels = Select(self.driver.find_element_by_id('levels'))
-    #     levels.select_by_visible_text('Undergraduate')
-    #     time.sleep(1)
-    #     self.driver.implicitly_wait(5)
-
-    #     # Select starting year
-    #     years = Select(self.driver.find_element_by_id('years'))
-    #     years.select_by_visible_text('2022')
-    #     time.sleep(1)
-    #     self.driver.implicitly_wait(5)
-
-    #     # Select course to complete - depending on the three above options
-    #     course = Select(self.driver.find_element_by_id('courses'))
-    #     time.sleep(1)
-    #     self.driver.implicitly_wait(5)
-
-
-    #     self.assertEqual(1,1)
-
-
-    def test_courseplanner(self):
-
-        # Choose type of degree
-        levels = Select(self.driver.find_elements_by_name('levels')[1])
-
+        # Select level of study
+        levels = Select(self.driver.find_element_by_id('levels'))
         levels.select_by_visible_text('Undergraduate')
-        time.sleep(2)
+        time.sleep(1)
         self.driver.implicitly_wait(5)
 
-        # Choose degree you want to study
-        major = Select(self.driver.find_elements_by_id('major_select')[0])
-        major.select_by_visible_text('Computer Science')
-        time.sleep(2)
+        # Select starting year
+        years = Select(self.driver.find_element_by_id('years'))
+        years.select_by_visible_text('2022')
+        time.sleep(1)
         self.driver.implicitly_wait(5)
+
+        # Select course to complete - depending on the three above options
+        courses = Select(self.driver.find_element_by_id('courses'))
+        courses.select_by_visible_text('Bachelor of Science')
+        time.sleep(1)
+        self.driver.implicitly_wait(5)
+
+        # Scroll down
+        self.driver.execute_script("window.scrollTo(0, 500)")
+        time.sleep(3) 
+
+        # Check the annual fee and typical fee range
+        ANF = self.driver.find_element_by_id('ANF')
+        TFR = self.driver.find_element_by_id('TFR')
+        self.assertEqual(ANF.get_attribute('innerHTML'), '12600')
+        self.assertEqual(TFR.get_attribute('innerHTML'), '37800')
+
+
+
+    # def test_courseplanner(self):
+
+    #     # Choose type of degree
+    #     levels = Select(self.driver.find_elements_by_name('levels')[1])
+
+    #     levels.select_by_visible_text('Undergraduate')
+    #     time.sleep(2)
+    #     self.driver.implicitly_wait(5)
+
+    #     # Choose degree you want to study
+    #     major = Select(self.driver.find_elements_by_id('major_select')[0])
+    #     major.select_by_visible_text('Computer Science')
+    #     time.sleep(2)
+    #     self.driver.implicitly_wait(5)
 
 
 if __name__=='__main__':
-  unittest.main(verbosity=2)
+    unittest.main()
 
 
